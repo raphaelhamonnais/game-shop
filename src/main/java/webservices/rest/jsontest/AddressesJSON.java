d389e3e28080cc2cef79179bfa94328691c0476b
raphaelhamonnais <raphael.hamonnais@gmail.com>
2017-04-30 15:20:40 +0200

    Add AddressesJSON.java example

---
+++ src/main/java/webservices/rest/jsontest/AddressesJSON.java
@@ -0,0 +1,58 @@
+package webservices.rest.jsontest;
+
+import model.Address;
+import model.Orders;
+import model.Users;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
+import java.util.Collection;
+import java.util.Collections;
+
+
+@Path("json-test/address")
+public class AddressesJSON {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Address address(@Context SecurityContext sc) {
+        Address address = new Address();
+        address.setAdrName("address name");
+        address.setAdrCity("Paris");
+        address.setAdrStreet("136 rue saint honoré");
+        address.setAdrZipCode("75001");
+        address.setAdrCountry("France");
+        return address;
+    }
+
+
+    @GET
+    @Path("users")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Collection<Users> users(@Context SecurityContext sc) {
+        Address address = new Address();
+        address.setAdrName("address name");
+        address.setAdrCity("Paris");
+        address.setAdrStreet("136 rue saint honoré");
+        address.setAdrZipCode("75001");
+        address.setAdrCountry("France");
+        return address.getUsers();
+    }
+
+    @GET
+    @Path("orders")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Collection<Orders> orders(@Context SecurityContext sc) {
+        Address address = new Address();
+        address.setAdrName("address name");
+        address.setAdrCity("Paris");
+        address.setAdrStreet("136 rue saint honoré");
+        address.setAdrZipCode("75001");
+        address.setAdrCountry("France");
+        return address.getOrders();
+    }
+}
