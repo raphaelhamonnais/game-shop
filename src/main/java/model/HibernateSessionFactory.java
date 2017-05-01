9d81d842912f7a4cf5c8017ef24fa4867cb13946
raphaelhamonnais <raphael.hamonnais@gmail.com>
2017-05-01 19:12:41 +0200

    Add HibernateSessionFactory that creates the hibernate session factory from "hibernate.cfg.xml" configuration file (singleton type of creation)

---
+++ src/main/java/model/HibernateSessionFactory.java
@@ -0,0 +1,27 @@
+package model;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateSessionFactory {
+
+    private static final String HIBERNATE_CFG_XML = "hibernate.cfg.xml";
+
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null)
+            createHibernateSessionFactory();
+        return sessionFactory;
+    }
+
+    private static void createHibernateSessionFactory() {
+        Configuration hibernateConf = new Configuration();
+        hibernateConf.configure(HIBERNATE_CFG_XML);
+        sessionFactory = hibernateConf.buildSessionFactory();
+    }
+
+
+
+
+}
