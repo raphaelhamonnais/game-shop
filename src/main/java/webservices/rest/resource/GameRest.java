106a1c2e8adc023385e349afc2801526a57c6e6d
raphaelhamonnais <raphael.hamonnais@gmail.com>
2017-06-02 10:05:00 +0200

    Add search function for games : search by name, console, category, isBest, isOnSale, isHot and isNew. All parameters are independent (you can search without specifying any of them or some of them or all of them)

--- src/main/java/webservices/rest/resource/GameRest.java
+++ src/main/java/webservices/rest/resource/GameRest.java
@@ -1,6 +1,5 @@
 package webservices.rest.resource;
 
-import model.entity.Console;
 import model.entity.Game;
 import model.handler.HibernateTransactionHandler;
 import model.query.QueryHandler;
@@ -10,7 +9,9 @@ import webservices.rest.PaginationService;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @Path("games")
@@ -68,6 +69,74 @@ public class GameRest extends PaginationService {
                 .createQuery(QueryHandler.Game.GET_PHYSICAL_GAMES)
                 .addParameter(QueryHandler.Game.ID_PARAMETER, id)
                 .getResultListAndClose();
+        return Response.status(Response.Status.OK).entity(result).build();
+    }
+
+
+    @GET
+    @Path("search")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getGamesSearch(
+            @QueryParam("name") String name,
+            @QueryParam("console") String console,
+            @QueryParam("category") String category,
+            @QueryParam("isBest") String isBest,
+            @QueryParam("isNew") String isNew,
+            @QueryParam("isHot") String isHot,
+            @QueryParam("isOnSale") String isOnSale) {
+
+        StringBuilder query = new StringBuilder()
+                .append("from Game ga join fetch ga.publisher join fetch ga.categories cat join fetch ga.physicalGames pg join fetch pg.console con where 1=1 ");
+
+
+        Map<String, String> params = new HashMap<>();
+        params.put("name", name);
+        params.put("console", console);
+        params.put("category", category);
+        params.put("isBest", isBest);
+        params.put("isNew", isNew);
+        params.put("isHot", isHot);
+        params.put("isOnSale", isOnSale);
+        Map<String, String> queries = new HashMap<>();
+        queries.put("name", " and ga.gameName LIKE :name ");
+        queries.put("console", " and con.consoleName LIKE :console ");
+        queries.put("category", " and cat.catName LIKE :category ");
+        queries.put("isBest", " and ga.gameIsBest=true ");
+        queries.put("isNew", " and ga.gameIsNew=true ");
+        queries.put("isHot", " and ga.gameIsHot=true ");
+        queries.put("isOnSale", " and ga.gameIsOnSale=true ");
+
+
+        boolean queryHasParam = false;
+        for (String paramKey : params.keySet()) {
+            queryHasParam = params.get(paramKey) != null;
+            if (queryHasParam)
+                break;
+        }
+
+        if (!queryHasParam) {
+            System.out.println("No params => get all games");
+            return this.getAll();
+
+        }
+
+
+        for (String paramKey : params.keySet())
+            if (params.get(paramKey) != null)
+                query.append(queries.get(paramKey));
+
+        HibernateTransactionHandler tx = new HibernateTransactionHandler()
+                .openSession()
+                .createQuery(query.toString());
+        if (params.get("name") != null)
+            tx = tx.addParameter("name", "%" + name + "%");
+        if (params.get("console") != null)
+            tx = tx.addParameter("console", "%" + console + "%");
+        if (params.get("category") != null)
+            tx = tx.addParameter("category", "%" + category + "%");
+
+
+        List result = tx.getResultListAndClose();
         return Response.status(Response.Status.OK).entity(result).build();
     }
 
