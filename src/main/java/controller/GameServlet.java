d7cf46f87e27cd1f725174bd9dc1fd49ce70b649
raphaelhamonnais <raphael.hamonnais@gmail.com>
2017-06-18 15:02:41 +0200

    XSS injection security

--- src/main/java/controller/GameServlet.java
+++ src/main/java/controller/GameServlet.java
@@ -15,18 +15,19 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
+import static org.apache.commons.lang3.StringEscapeUtils.escapeHtml4;
 @WebServlet("/games")
 public class GameServlet extends HttpServlet{
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
         try {
             HttpResponse<JsonNode> games = new GameClient().search(
-                    request.getParameter("gameName"),
-                    request.getParameter("console"),
-                    request.getParameter("category"),
-                    request.getParameter("isBest"),
-                    request.getParameter("isNew"),
-                    request.getParameter("isHot"),
-                    request.getParameter("isOnSale")
+                    escapeHtml4(request.getParameter("gameName")),
+                    escapeHtml4(request.getParameter("console")),
+                    escapeHtml4(request.getParameter("category")),
+                    escapeHtml4(request.getParameter("isBest")),
+                    escapeHtml4(request.getParameter("isNew")),
+                    escapeHtml4(request.getParameter("isHot")),
+                    escapeHtml4(request.getParameter("isOnSale"))
             );
 
             List<Game> gamesList = new JsonParser().parseJsonListOfObjects(games.getBody().toString(), Game[].class);
